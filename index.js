cac632595fb532d1bc7e67ae0c93c5c8534b7083
nick4335711 <nicholasocran191@gmail.com>
2020-08-04 15:43:10 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+function getDate(){
+    myBody.appendChild(myPara);
+}
+
+const myPara = document.createElement('p');
+myPara.textContent = Date();
+
+const myBody = document.getElementById('mainBody');
